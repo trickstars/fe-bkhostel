8b7d598aea9e5ab3d93fa88f39ccec7da86dc384
Hien Giang <quachtrieuvu@gmail.com>
2023-11-22 19:17:57 +0700

    add auth pages, react-hook-form & tanstack-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import router from './routes/index';
+export const queryClient = new QueryClient();
+
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
-import UserLayout from "../layouts/UserLayout";
-import AdminLayout from "../layouts/AdminLayout";
-import ErrorPage from "../pages/error/Error";
-import HomePage from "../pages/home/Home";
-import LoginPage from "../pages/login/Login";
-
+import { createBrowserRouter } from 'react-router-dom';
+import UserLayout from '../layouts/UserLayout';
+import AdminLayout from '../layouts/AdminLayout';
+import ErrorPage from '../pages/error/Error';
+import HomePage from '../pages/home/Home';
+import LoginPage from '../pages/login/Login';
+import RegisterPage from '../pages/register/Register';
+import EnterEmailPage from '../pages/enter-email/EnterEmail';
+import SendPasswordPage from '../pages/send-password/SendPassword';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <UserLayout />,
-    errorElement:<ErrorPage />, 
-    children: [
-      {index: true, element: <HomePage />}
-    ]
+    errorElement: <ErrorPage />,
+    children: [{ index: true, element: <HomePage /> }],
   },
   {
     path: '/login',
     element: <LoginPage />,
     errorElement: <ErrorPage />,
-  }
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/enter-email',
+    element: <EnterEmailPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/send-password',
+    element: <SendPasswordPage />,
+    errorElement: <ErrorPage />,
+  },
   // Admin routes will be updated soon
 ]);
 
