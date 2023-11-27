21f941dff3ec2f8b0dc8faf7974106ad5086caa8
duyphatbk <thoiduyphat.p@gmail.com>
2023-11-27 11:07:27 +0700

    paging

--- src/pages/user-detail/components/left-side-bar.jsx
+++ src/pages/user-detail/components/left-side-bar.jsx
@@ -9,7 +9,7 @@ import { RiArrowDownSFill } from "react-icons/ri";
 import 'flowbite';
 
 const style = {
-    color: '#25BEB9', fontSize: '1.5rem'
+    color: '#25BEB9', fontSize: '100%'
 }
 const style_list = "flex flex-row items-center pl-3 ml-2 hover:bg-[#B6D6F2] rounded-3xl";
 const LeftSideBar = () => {
@@ -20,8 +20,8 @@ const LeftSideBar = () => {
 
     }
     return (
-        <div className=" h-screen col-start-1 col-span-2 pl-3 text-lg bg-white">
-            <div className="my-8 ml-4 mb-3 max-w-[320px] hover:cursor-pointer text-4xl font-semibold text-[#0891B2]">BKHOSTEL</div>
+        <div className=" h-screen col-start-1 col-span-2 pl-3 lg:text-lg md:text-md sm:text-sm bg-white">
+            <div className="my-8 ml-4 mb-3 hover:cursor-pointer lg:text-4xl md:text-3xl font-semibold text-[#0891B2]">BKHOSTEL</div>
             <div className={style_list}>
                 <PiTelevision style={style} />
                 <div className="my-8 hover:cursor-pointer ml-2 ">Statistics</div>
--- src/pages/user-detail/user-list.jsx
+++ src/pages/user-detail/user-list.jsx
@@ -1,43 +1,32 @@
-import { RxAvatar } from "react-icons/rx";
-import { LiaFlagUsaSolid } from "react-icons/lia";
+
 import { FaCamera } from "react-icons/fa";
 import 'flowbite';
 import LeftSideBar from "./components/left-side-bar";
-
+import Header from "./components/header";
 const UserList = () => {
     return (
         <div className="grid grid-cols-10 gap-3">
             <LeftSideBar />
             <div className="h-screen col-start-3 col-span-8 pl-3 text-lg bg-[#b6d6f2]">
-                <div className="flex align-center items-center h-24">
-                    <div class="relative w-80 ml-14">
-                        <input type="search" id="search-dropdown" class="block p-2.5 w-full text-sm text-black-900 bg-gray-50 rounded-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
-                        <button type="submit" class="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-[#E8C4F2] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-6 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg></button>
-                    </div>
-                    <div className="ml-96 pl-72 flex flex-row align-center items-center  ">
-                        <LiaFlagUsaSolid style={{ cursor: "pointer", fontSize: "35px", color: 'blue' }} />
-                        <RxAvatar style={{ cursor: "pointer", fontSize: "35px", marginLeft: "20px" }} />
-                    </div>
-                </div>
+                <Header />
                 <div className="my-8 ml-14 mt-8 hover:cursor-pointer text-2xl font-semibold text-black">
                     UserList/Duc Phan
                 </div>
                 <div className="grid grid-cols-3 mx-14 bg-white text-[#797C7B] p-6 rounded-2xl">
-                    <div className="pr-4">
-                        <div className="rounded-full bg-[#A6D4AE] md:w-64 md:h-64 mx-auto relative z-0 ">
-                            <div className="absolute ml-48 z-10 mt-52 p-1.5 w-fit border bg-[#D9D9D9] rounded-full">
-                                <FaCamera style={{ color: '#f5a201', fontSize: '24px' }} />
-                            </div>
+                    <div className="pr-4 flex flex-col">
+                        <div className="rounded-full bg-[#A6D4AE] xl:w-64 xl:h-64 lg:w-52 lg:h-52 md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto absolute z-0 ">
+                            
                         </div>
-                        <div>
-                            <button type="submit" class="mt-10 w-full text-white bg-[#00537A] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Order History</button>
+                        <div className="relative xl:ml-48 xl:mt-52 lg:ml-36 lg:mt-44 md:ml-24 md:mt-28 sm:ml-14 sm:mt-20 z-10 p-1.5 w-fit border bg-[#D9D9D9] rounded-full">
+                                <FaCamera style={{ color: '#f5a201'}} />
+                            </div>
+                        <div className="relative">
+                            <button type="submit" class="mt-14 w-full text-white bg-[#00537A] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Order History</button>
                             <button type="submit" class="mt-2 w-full text-white bg-[#00537A] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Change Password</button>
                         </div>
                     </div>
                     <form className="col-start-2 col-span-3">
-                        <div class="grid gap-6 mb-10 md:grid-cols-2">
+                        <div class="grid gap-6 mb-10 lg:grid-cols-2">
                             <div>
                                 <input type="text" id="first_name" class="bg-gray-50 border border-[#B6D6F2] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First Name *" required />
                             </div>
@@ -45,7 +34,7 @@ const UserList = () => {
                                 <input type="text" id="last_name" class="bg-gray-50 border border-[#B6D6F2] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last Name *" required />
                             </div>
                             <div>
-                                <input type="text" id="company" class="bg-gray-50 border border-[#B6D6F2] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Address *" required />
+                                <input type="email" id="company" class="bg-gray-50 border border-[#B6D6F2] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Address *" required />
                             </div>
                             <div>
                                 <input type="tel" id="phone" class="bg-gray-50 border border-[#B6D6F2] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Phone Number *" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
