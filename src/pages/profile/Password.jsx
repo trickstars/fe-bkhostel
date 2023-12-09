48cc4da1305065a66d22753ca0c67c0cfa5c6926
TinLy <lychanhtintv@gmail.com>
2023-12-10 00:24:21 +0700

    update and fix

---
+++ src/pages/profile/Password.jsx
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import { loginUser } from '../../utils/users';
+
+const baseURL = import.meta.env.VITE_BACKEND_API + '/users';
+
+const Password = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const action = props.action
+    // eslint-disable-next-line react/prop-types
+    const config = props.config
+
+    // eslint-disable-next-line react/prop-types
+    const changeConfig = props.changeConfig
+
+    const [isPasswordChange, setIsPasswordChange] = useState(false)
+    const [username, setUsername] = useState("")
+
+    const handleChangePassword = (e) => {
+        setIsPasswordChange(false);
+        e.preventDefault()
+        const passwordData = {
+            oldPassword: e.target.oldPassword.value,
+            newPassword: e.target.newPassword.value,
+            confirmPassword: e.target.confirmPassword.value
+        }
+
+        changePassword(passwordData)
+        login(e)
+    }
+    const login = async (passwordData) => {
+        const response = await loginUser({
+            // eslint-disable-next-line react/prop-types
+            username: username,
+            password: passwordData.newPassword
+        });
+        localStorage.setItem('token', response.token)
+
+        changeConfig({'Authorization': response.token})
+    }
+
+    const getUsername = async () => {
+        console.log("get Username")
+        await axios.get(`${baseURL}/`, {headers: config}).then(res => setUsername(res.data.username));
+
+    };
+
+    const changePassword = async (passwordData) => {
+        console.log(passwordData);
+        try {
+            await axios.post(`${baseURL}/change-password`, passwordData, {headers: config}).then(()=>setIsPasswordChange(true)).then(() => login(passwordData));
+        } catch (error) {
+            const customError = new Error();
+            customError.message = error.response.data.message;
+            console.log(customError.message);
+            throw customError;
+        }
+    };
+    useEffect(() => {
+        getUsername()
+    },[])
+
+    return (
+            <div className="flex flex-col row-start-1 row-span-7 col-start-3 col-span-6 ml-5">
+                <div className="py-4 mt-6 border-b border-gray rounded-md h-fit text-5xl">
+                    Chỉnh sửa thông tin cá nhân
+                </div>
+                <form id="form2" className="my-10 mr-10" onSubmit={handleChangePassword}>
+                    <div className="items-start flex justify-between mt-8">
+                        <label className="inline-block [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[24px] tracking-[0] leading-[normal]" htmlFor="oldPassword">Mật khẩu cũ</label>
+                        <input className="px-3 inline-block float-right w-2/3 h-10 outline-none rounded-[4px] border border-solid border-[#cccccc]" type="password" id="oldPassword" defaultValue="" name="oldPassword"/>
+                    </div>
+                    <div className="items-start flex justify-between mt-8">
+                        <label className="inline-block [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[24px] tracking-[0] leading-[normal]" htmlFor="newPassword">Mật khẩu mới</label>
+                        <input className="px-3 inline-block float-right w-2/3 h-10 outline-none rounded-[4px] border border-solid border-[#cccccc]" type="password" id="newPassword" defaultValue="" name="newPassword"/>
+                    </div>
+                    <div className="items-start flex justify-between mt-8">
+                        <label className="inline-block [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[24px] tracking-[0] leading-[normal]" htmlFor="confirmPassword">Xác nhận mật khẩu mới</label>
+                        <input className="px-3 inline-block float-right w-2/3 h-10 outline-none rounded-[4px] border border-solid border-[#cccccc]" type="password" id="confirmPassword" defaultValue="" name="confirmPassword"/>
+                    </div>
+                    <div className='flex justify-between'>
+                        <button type="button" onClick={() => action()} className='justify-center items-center w-full rounded-md px-3 py-3 mt-8 mb-16 mr-8 bg-[#4992FF]'>
+                                <p className='text-sm text-white text-3xl font-semibold'>Quay lại</p>
+                        </button>
+                        <button type="submit" className='justify-center items-center w-full rounded-md px-3 py-3 mt-8 mb-16 ml-8 bg-[#16AD4D]'>
+                            <p className='text-sm text-white text-3xl font-semibold'>Cập nhật</p>
+                        </button>
+                    </div>
+                    <div id="alert-4" className={isPasswordChange? " border-4 border-green-300 flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400": "hidden"} role="alert">
+                        <div className="ms-3 text-sm font-medium">
+                            Mật khẩu đã được thay đổi thành công
+                        </div>
+                        <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-4" aria-label="Close">
+                            <span className="sr-only">Close</span>
+                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+    )
+
+}
+export default Password;
--- src/pages/services-table/ServicesTable.jsx
+++ src/pages/services-table/ServicesTable.jsx
@@ -1,120 +1,213 @@
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
+const servicesURL = import.meta.env.VITE_BACKEND_API + '/services';
+
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 const ServicesTable = () => {
-  return (
+    const [services , setServices] = useState([''])
+    const avantages = [
+        "- Lượt xem nhiều gấp 30 lần so với tin thường",
+        "- Ưu việt, tiếp cận tối đa khách hàng.",
+        "- Xuất hiện vị trí đầu tiên ở trang chủ",
+        "- Đứng trên tất cả các loại tin VIP khác",
+        "- Xuất hiện đầu tiên ở mục tin nổi bật xuyên suốt khu vực chuyên mục đó.",
+        "- Lượt xem nhiều gấp 30 lần so với tin thường",
+        "- Phù hợp khách hàng là công ty/cá nhân sở hữu hệ thống lớn có từ 15-20 căn phòng/nhà trở lên hoặc phòng trống quá lâu, thường xuyên đang cần cho thuê gấp.",
+    ]
+
+    const suitable = [
+        "- Phù hợp khách hàng là công ty/cá nhân sở hữu hệ thống lớn có từ 15-20 căn phòng/nhà trở lên hoặc phòng trống quá lâu, thường xuyên đang cần cho thuê gấp."
+    ]
+
+    const getServices = async () => {
+        console.log("get Services")
+        try {
+            await axios.get(`${servicesURL}`).then(res => setServices(res.data));
+        } catch (error) {
+            const customError = new Error();
+            customError.message = error.response.data.message;
+            console.log(customError.message);
+            throw customError;
+        }
+
+    };
+
+    useEffect(() => {
+        getServices();
+    }, [])
+    return (
     <div className="">
-      <div className="max-w-[1536px] mx-auto m-10">
-        <p className="font-bold text-center text-4xl">Bảng giá dịch vụ</p>
+        <div className="max-w-[1536px] mx-auto m-10">
+            <p className="font-bold text-center text-4xl">Bảng giá dịch vụ</p>
+            <table className="table-auto border-collapse border border-slate-300 flex mt-10">
+                <thead className="grid font-bold rounded-[4px] bg-[#ffff] w-1/6">
+                    <td className="grid grid-row-16 font-bold rounded-[4px]">
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10">
+                            <span className="text-[#fff]">Loại</span>
+                        </tr>
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10 h-96">
+                            <span className="text-[#fff]">Ưu điểm</span>
+                        </tr>
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10 h-48">
+                            <span className="text-[#fff]">Phù hợp</span>
+                        </tr>
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10 h-12">
+                            <span className="text-[#fff]">Giá ngày</span>
+                        </tr>
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10 h-12">
+                            <span className="text-[#fff]">Giá tuần</span>
+                        </tr>
+                        <tr className="m-2 bg-[#9DC4FF] justify-center items-center flex p-3 px-10 h-12">
+                            <span className="text-[#fff]">Giá tháng</span>
+                        </tr>
+                    </td>
+                </thead>
+                <tbody className="flex justify-around w-5/6">
+                {services.length ? services.map((item, idx)=> {
+                    return (
+                        <td key={idx} className="grid">
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#FF4E4E]">
+                                <span className="text-[#fff] w-full">VIP {item.category}</span>
+                            </tr>
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#F3F4FF] h-96">
+                                <span className="text-[#000] w-full">
+                                    <p>{avantages[0]}</p>
+                                    <p>{avantages[1]}</p>
+                                    <p>{avantages[2]}</p>
+                                    <p>{avantages[3]}</p>
+                                    <p>{avantages[4]}</p>
+                                    <p>{avantages[5]}</p>
+                                </span>
+                            </tr>
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#F3F4FF] h-48">
+                                <span className="text-[#000] w-full">
+                                    <p>{suitable[0]}</p>
+                                </span>
+                            </tr>
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#F3F4FF] h-12">
+                                <span className="text-[#000] w-full">{item.daily_price} đồng/ ngày</span>
+                            </tr>
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#F3F4FF] h-12">
+                                <span className="text-[#000] w-full">{item.weekly_price} đồng/ tuần</span>
+                            </tr>
+                            <tr className="m-2 border border-slate-300 p-3 bg-[#F3F4FF] h-12">
+                                <span className="text-[#000] w-full">{item.monthly_price} đồng/ tháng</span>
+                            </tr>
+                        </td>
+                    )
+                }):""}
+                </tbody>
+            </table>
 
-        <div className="flex self-center justify-between mt-8">
-          <div className="w-1/5 h-24 bg-[#ffff] justify-center items-center flex m-2">
-            <span className="text-[#fff]"></span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
-            <span className="text-[#fff]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
-            <span className="text-[#fff]">VIP 2</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
-            <span className="text-[#fff]">VIP 3</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
-            <span className="text-[#fff]">VIP 4</span>
-          </div>
+            {/* <div className="flex self-center justify-between mt-8">
+                <div className="w-1/5 h-24 bg-[#ffff] justify-center items-center flex m-2">
+                    <span className="text-[#fff]"></span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
+                    <span className="text-[#fff]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
+                    <span className="text-[#fff]">VIP 2</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
+                    <span className="text-[#fff]">VIP 3</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#FF0000] justify-center items-center flex m-2">
+                    <span className="text-[#fff]">VIP 4</span>
+                </div>
+            </div>
+
+            <div className="flex self-center justify-between">
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">Ưu điểm</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+            </div>
+
+            <div className="flex self-center justify-between">
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">Phù hợp</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+            </div>
+
+            <div className="flex self-center justify-between">
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">Giá ngày</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+            </div>
+
+            <div className="flex self-center justify-between">
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">Giá tuần</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+            </div>
+
+            <div className="flex self-center justify-between">
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">Giá tháng</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+                <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
+                    <span className="text-[#000]">VIP 1</span>
+                </div>
+            </div> */}
         </div>
 
-        <div className="flex self-center justify-between">
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">Ưu điểm</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-        </div>
-
-        <div className="flex self-center justify-between">
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">Phù hợp</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-        </div>
-
-        <div className="flex self-center justify-between">
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">Giá ngày</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-        </div>
-
-        <div className="flex self-center justify-between">
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">Giá tuần</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-        </div>
-
-        <div className="flex self-center justify-between">
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">Giá tháng</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-          <div className="w-1/5 h-24 bg-[#ccc] justify-center items-center flex mx-2 my-1">
-            <span className="text-[#000]">VIP 1</span>
-          </div>
-        </div>
-      </div>
     </div>
-  );
-};
+    )
+}
 export default ServicesTable;
